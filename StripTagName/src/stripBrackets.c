927fd1a7c52b92d6abf24fab803ca18edda7662e
hmcaio <hmcaio@hotmail.com>
2016-01-28 12:25:33 -0500

    StripTagName setup.

---
+++ StripTagName/src/stripBrackets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+void stripTagName(char* tag);
+
+
+int main(int argc, char* argv[])
+{
+    if (argc <= 1)
+    {
+        printf("Give me a tag.\n");
+        return 1;
+    }
+    
+    stripTag(argv[1]);
+    printf("[%s]\n", argv[1]);
+    
+    return 0;
+}
+
+void stripTagName(char* tag)
+{
+    if (!tag)
+        return;
+    
+    int i = 0, j = 0;
+    while (tag[i] != ' ' && tag[i] != '>')
+    {
+        if (tag[i] != '<' && tag[i] != '/')
+            tag[j++] = tag[i];
+        i++;
+    }
+    tag[j] = '\0';
+}
